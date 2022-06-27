6bac49dcf8178c3efb3c2dd1ff3167139fb0626b
Gabriel Ramos <ramos.gabriel@hotmail.com.br>
2022-06-27 14:49:16 -0300

    Section

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -21,3 +21,9 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha Principal do Curso', 1, 'https://upload.wikimedia.org/wikipedia/commons/5/54/ESL_online_learning.jpg', 1, 1); 
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Fórum HTML', 'Tire suas dúvidas', 2, 'https://upload.wikimedia.org/wikipedia/commons/5/54/ESL_online_learning.jpg', 2, 1); 
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://upload.wikimedia.org/wikipedia/commons/5/54/ESL_online_learning.jpg', 0, 1); 
+
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://upload.wikimedia.org/wikipedia/commons/5/54/ESL_online_learning.jpg', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos continuar', 2, 'https://upload.wikimedia.org/wikipedia/commons/5/54/ESL_online_learning.jpg', 1,1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos finalizar', 3, 'https://upload.wikimedia.org/wikipedia/commons/5/54/ESL_online_learning.jpg', 1, 2);
+
+
